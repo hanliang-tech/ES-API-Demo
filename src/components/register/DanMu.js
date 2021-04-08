1f3cc44891ca8223a4e3bb993b5896b7bea3b4ed
weipeng <weipeng@bftv.com>
2021-04-08 15:54:21 +0800

    Add DM

--- src/components/register/DanMu.js
+++ src/components/register/DanMu.js
@@ -9,10 +9,25 @@ function registerDanMuView() {
   Vue.component('danmu-view',{
     methods: {
       addDM(data, callback) {
-        Vue.Native.callUIFunction(this.$refs.danmu, 'addDM', data, (res) => {
+        Vue.Native.callUIFunction(this.$refs.danmu, 'addDM', [data], (res) => {
           callback(res)
         });
-      }
+      },
+      pause(data, callback) {
+        Vue.Native.callUIFunction(this.$refs.danmu, 'pause', [data], (res) => {
+          callback(res)
+        });
+      },
+      resume(data, callback) {
+        Vue.Native.callUIFunction(this.$refs.danmu, 'resume', [data], (res) => {
+          callback(res)
+        });
+      },
+      release(data, callback) {
+        Vue.Native.callUIFunction(this.$refs.danmu, 'release', [data], (res) => {
+          callback(res)
+        });
+      },
     },
     render(h) {
       return h('DanMuView',
