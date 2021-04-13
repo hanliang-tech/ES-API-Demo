d7a7b2a164253d339d918f62b6a93cb8ec41d19e
weipeng <weipeng@bftv.com>
2021-04-13 11:27:33 +0800

    二维码优化

--- src/components/es-tv-demos/img-qr.vue
+++ src/components/es-tv-demos/img-qr.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container" style="width:1920px; height:1080px; display:flex; flex-direction:row; justify-content:center; align-items:center">
       <!-- content: 二维码要显示的内容 -->
-    <qr-view style="width:200px; height:200px;" content="http://www.xxxx.com/"/>
+    <qr-view style="width:200px; height:200px;" content="http://www.xxxx.com/" />
+    <div style="width:100px; "/>
+    <qr-view style="width:200px; height:200px;" content="http://www.xxxx.com/" :optimize=false />
   </div>
 </template>
 
